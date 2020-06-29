43e0918fae6243912a79fcc6807faf5f00b5c9f0
MSTAR45 <66040005+MSTAR45@users.noreply.github.com>
2020-06-29 15:27:05 -0400

    Create star45.py

---
+++ star45.py
@@ -0,0 +1,6 @@
+#coding: utf-8
+print("hello Dear User")
+print("WELCOME i'm @Star45_manager_bot")
+print("You Can contact me On Telegram ")
+print("Contact Me On ⭐ t.me/star45_manager_bot")
+print("And Join Me On t.me/Kingmstar45")
